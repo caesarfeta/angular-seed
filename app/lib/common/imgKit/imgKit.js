9b74f79c70696cdaf6ec04e05ecde371a6b27613
caesarfeta <adamtavares@gmail.com>
2016-02-13 19:42:43 -0500

    Added imgKit launcher

--- app/lib/common/imgKit/imgKit.js
+++ app/lib/common/imgKit/imgKit.js
@@ -5,20 +5,36 @@ define([
 'jquery'
 ], 
 function( angular, $ ){
-	
-	angular.module( 'imgKit', [] )
-	
-	// run function on input enter
-	
-	.directive( 'imgKit', function(){
-			return {
-				template: '<div class="img-kit-menu"><ng-transclude></ng-transclude></div>',
-				replace: true,
-				transclude: true,
-				link: function( scope, elem ){
-					
-				}
-			}
-	})
+  
+  angular.module( 'imgKit', [] )
+  
+  // run function on input enter
+  
+  .directive( 'imgKit', [ 
+      '$location', 
+      function( $location ){ return {
+          template: [
+            
+            '<div class="img-kit">',
+              '<span class="launcher">',
+                '<i ng-repeat="( key, url ) in toCanvas" ng-click="launch( key )" class="fa fa-{{ key }}"></i>',
+              '</span>',
+              '<ng-transclude></ng-transclude>',
+            '</div>'
+            
+          ].join(''),
+          replace: true,
+          transclude: true,
+          link: function( scope, elem ){
+          
+            scope.toCanvas = {
+              'bomb': 'bomb'
+            };
+          
+            scope.launch = function( key ){
+              $location.path( '/canvas/'+scope.toCanvas[ key ] );
+            };
+          }
+			}}
+  ])
 })
-	
