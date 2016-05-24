05bc30ad99d081741788c4f3c15580cfce95a353
Adam Tavares <atavares@nd.com>
2016-05-24 15:54:33 -0400

    remove external templates

--- app/lib/dbpedia/script/dbpSpecies.js
+++ app/lib/dbpedia/script/dbpSpecies.js
@@ -2,21 +2,41 @@ define([
 '../module',
 ], 
 function( module ){
-	module.directive( 'dbpSpecies', [
-		'dbpediaSvc',
-		function( dbpedia ){
-			return {
-				restrict: 'E',
-				replace: true,
-				scope: {},
-				templateUrl: 'lib/dbpedia/bio/species.html',
-				link: function( scope, elem ){
-					scope.dbpedia = dbpedia;
-					scope.on = function(){
-						return dbpedia.img.result != null && dbpedia.img.result.length > 0 && !dbpedia.waiting
-					}
-				}
-			}
-		}
-	])
+    module.directive( 'dbpSpecies', [
+        'dbpediaSvc',
+        function( dbpedia ){
+            return {
+                restrict: 'E',
+                replace: true,
+                scope: {},
+                template: [
+                    
+                    '<div class="dbpedia-bio-species-bg" ng-show="on()">',
+                        '<div class="dbpedia-bio-species clearfix"',
+                             'ng-repeat="species in dbpedia.img.result">',
+                            '<h2 class="name" ng-bind-html="species.name.value | highlight:dbpedia.img.search"></h2>',
+                            '<div img-kit="{{ species.img.value }}"><img ng-src="{{ species.img.value }}"/></div>',
+                            '<table class="taxonomy">',
+                                '<tr><td>Kingdom</td><td>{{ species.kingdom.value | shrinkLink }}</td></tr>',
+                                '<tr><td>Phylum</td><td>{{ species.phylum.value | shrinkLink }}</td></tr>',
+                                '<tr><td>Class</td><td>{{ species.class.value | shrinkLink }}</td></tr>',
+                                '<tr><td>Order</td><td>{{ species.order.value | shrinkLink }}</td></tr>',
+                                '<tr><td>Family</td><td>{{ species.family.value | shrinkLink }}</td></tr>',
+                                '<tr><td>Genus</td><td>{{ species.genus.value | shrinkLink }}</td></tr>',
+                                '<tr><td>Species</td><td>{{ species.species.value | shrinkLink }}</td></tr>',
+                            '</table>',
+                            '<div class="abstract" ng-bind-html="species.abstract.value | highlight:dbpedia.img.search"></div>',
+                        '</div>',
+                    '</div>'
+                    
+                ].join(' '),
+                link: function( scope, elem ){
+                    scope.dbpedia = dbpedia;
+                    scope.on = function(){
+                        return dbpedia.img.result != null && dbpedia.img.result.length > 0 && !dbpedia.waiting
+                    }
+                }
+            }
+        }
+    ])
 })
