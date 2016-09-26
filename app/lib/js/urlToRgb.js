00a22837f3fc08c581bbf7fdaaf146a28479f6db
Adam Tavares <atavares@nd.com>
2016-09-26 15:28:38 -0400

    caching img data

--- app/lib/js/urlToRgb.js
+++ app/lib/js/urlToRgb.js
@@ -8,20 +8,41 @@ function( angular ){
   }])
   self.canvas = document.createElement( 'canvas' );
   self.ctx = self.canvas.getContext( '2d' );
+  
+  // cache img data
+  
+  self.cache = {};
+  function itemFromUrl( url ){
+    if ( !( url in self.cache )){
+        var item = { 
+        img: new Image(),
+        data: undefined
+      }
+      item.img.crossOrigin = 'anonymous';
+      self.cache[ url ] = item;
+    }
+    return self.cache[ url ]
+  }
+  
+  // get the img data
+  
   self.get = function( url ){
     return self.$q( function( yes, no ){
-      self.image = new Image();
-      self.image.crossOrigin = 'anonymous';
-      self.image.onload = function(){
-        self.canvas.setAttribute( 'width', self.image.width );
-        self.canvas.setAttribute( 'height', self.image.height );
-        self.ctx.drawImage( self.image, 0, 0 );
-        return yes( self.ctx.getImageData( 0, 0, self.canvas.width, self.canvas.height ))
+      var item = itemFromUrl( url )
+      if ( !!item.data ){
+        return yes( item.data )
       }
-      self.image.onerror = function( error ){
+      item.img.onload = function(){
+        self.canvas.setAttribute( 'width', item.img.width );
+        self.canvas.setAttribute( 'height', item.img.height );
+        self.ctx.drawImage( item.img, 0, 0 );
+        item.img.data = self.ctx.getImageData( 0, 0, self.canvas.width, self.canvas.height );
+        return yes( item.img.data )
+      }
+      item.img.onerror = function( error ){
         return no( error )
       }
-      self.image.src = 'http://localhost:5000/' + url;
+      item.img.src = 'http://localhost:5000/' + url;
     })
   }
   return self
