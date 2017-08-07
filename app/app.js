e3beb12525f542fdf15d8f459672ee59b1f2ec1e
caesarfeta <adamtavares@gmail.com>
2017-08-07 19:32:20 -0400

    centralizing route config

--- app/app.js
+++ app/app.js
@@ -27,9 +27,124 @@ function( angular ){
       $routeProvider.otherwise({ 
         redirectTo: '/specierch'
       })
+      
+      var plexvas = {
+        template: [
+          
+          '<div class="container">',
+            '<div class="row">',
+              '<div class="col-xs-9" i-canvas="iCanvas"></div>',
+              '<div class="col-xs-3" i-canvas-ctrl="iCanvas"></div>',
+            '</div>',
+          '</div>'
+          
+        ].join(' '),
+        controller: [
+          '$scope',
+          'iCanvas',
+          function(
+            $scope,
+            iCanvas ){
+            $scope.iCanvas = new iCanvas()
+          }]
+      }
+      $routeProvider.when('/plexvas', plexvas )
+      $routeProvider.when('/plexvas/:id', plexvas )
+      
+      // lsys
+      
+      $routeProvider.when( '/lsys/:id', {
+        template: [
+          
+          '<div lsys-sketch="id"></div>'
+          
+        ].join(' '),
+        controller: [
+          '$scope',
+          '$routeParams',
+          function( $scope, $routeParams ){
+            $scope.id = $routeParams.id
+          }
+        ]
+      })
+      
+      $routeProvider.when( '/lsys/list/:page', {
+        template: [
+          
+          '<div lsys-lib="page"></div>'
+          
+        ].join(' '),
+        controller: [
+          '$scope',
+          '$routeParams',
+          function(
+            $scope,
+            $routeParams ){
+              $scope.page = ( !!$routeParams.page ) ? $routeParams.page : 1
+          }
+        ]
+      })
+      
+      // fungi
+      
+      $routeProvider.when( '/fungi', {
+        template: [
+          
+          '<div class="fungi">',
+              '<div dbp-fungi-genus-list></div>',
+          '</div>'
+          
+        ].join(' '),
+        controller: function(){}
+      })
+      
+      $routeProvider.when( '/fungi/:genus*', {
+        template: [
+          
+          '<div class="fungi">',
+            '<div dbp-fungi-species-list></div>',
+          '</div>'
+          
+        ].join(' '),
+        controller: [ 
+          '$scope',
+          '$routeParams',
+          function( scope, $routeParams ){
+            scope.genus = $routeParams.genus
+          }
+        ]
+      })
+      
+      // about
+      
+      $routeProvider.when( '/about', {
+        template: [
+          
+          '<div class="about container">',
+            '<div class="row">',
+              '<div class="col-xs-2">',
+                '<img src="http://placehold.it/100x200" />',
+              '</div>',
+              '<div class="col-xs-10">',
+                '<p>{{ ::blurb }}</p>',
+                '<p>{{ ::other }}</p>',
+                '<p>{{ ::contact }}</p>',
+              '</div>',
+            '</div>',
+          '</div>'
+          
+        ].join(' '),
+        controller: [
+          '$scope',
+          function( $scope ){
+            $scope.blurb = 'Hello. My name is Adam Tavares. I am a programmer, artist, and nature enthusiast. I write software to explore data and generate mathematical art. I hope my site let\'s you explore Nature\'s wonderful shapes, colors, and patterns.'
+            $scope.other = 'In my free time I explore forests, draw cartoons, and play capoeira. I currently live in beautiful Providence, Rhode Island.'
+            $scope.contact = 'Contact me at adamtavares@gmail.com.'
+          }
+        ]
+      })
     }
   ])
-  
   angular.element().ready( 
     function(){
       
--- app/lib/canvas/canvas.js
+++ app/lib/canvas/canvas.js
@@ -4,112 +4,11 @@ define([
 'angularRoute'
 ],
 function( angular, viz ){
-  angular.module( 'about', [])
-  .config([
-    '$routeProvider',
-    function( $routeProvider ){
-      
-    }
-  ])
   
   angular.module( 'myApp.view.canvas', [
     'ngRoute',
     'lsys'
   ])
-  .config([
-    '$routeProvider', 
-    function( $routeProvider ){
-      
-      // plexvas
-      
-      var plexvas = {
-        template: [
-          
-          '<div class="container">',
-            '<div class="row">',
-              '<div class="col-xs-9" i-canvas="iCanvas"></div>',
-              '<div class="col-xs-3" i-canvas-ctrl="iCanvas"></div>',
-            '</div>',
-          '</div>'
-          
-        ].join(' '),
-        controller: [
-          '$scope',
-          'iCanvas',
-          function(
-            $scope,
-            iCanvas ){
-            $scope.iCanvas = new iCanvas()
-          }]
-      }
-      $routeProvider.when('/plexvas', plexvas )
-      $routeProvider.when('/plexvas/:id', plexvas )
-      
-      // lsys
-      
-      var lsysList = {
-        template: [
-          
-          '<div lsys-lib="page"></div>'
-          
-        ].join(' '),
-        controller: [
-          '$scope',
-          '$routeParams',
-          function(
-            $scope,
-            $routeParams ){
-              $scope.page = ( !!$routeParams.page ) ? $routeParams.page : 1
-          }
-        ]
-      }
-      var lsys = {
-        template: [
-          
-          '<div lsys-sketch="id"></div>'
-          
-        ].join(' '),
-        controller: [
-          '$scope',
-          '$routeParams',
-          function( $scope, $routeParams ){
-            $scope.id = $routeParams.id
-          }
-        ]
-      }
-      $routeProvider.when('/lsys/:id', lsys )
-      $routeProvider.when('/lsys/list/:page', lsysList )
-      
-      var about = {
-        template: [
-          
-          '<div class="about container">',
-            '<div class="row">',
-              '<div class="col-xs-2">',
-                '<img src="http://placehold.it/100x200" />',
-              '</div>',
-              '<div class="col-xs-10">',
-                '<p>{{ ::blurb }}</p>',
-                '<p>{{ ::other }}</p>',
-                '<p>{{ ::contact }}</p>',
-              '</div>',
-            '</div>',
-          '</div>'
-          
-        ].join(' '),
-        controller: [
-          '$scope',
-          function( $scope ){
-            $scope.blurb = 'Hello. My name is Adam Tavares. I am a programmer, artist, and nature enthusiast. I write software to explore data and generate mathematical art. I hope my site let\'s you explore Nature\'s wonderful shapes, colors, and patterns.'
-            $scope.other = 'In my free time I explore forests, draw cartoons, and play capoeira. I currently live in beautiful Providence, Rhode Island.'
-            $scope.contact = 'Contact me at adamtavares@gmail.com.'
-          }
-        ]
-      }
-      $routeProvider.when('/about', about )
-    }
-  ])
-  
   .service( 'iCanvasSvc', [
     function(){
       var self = this
