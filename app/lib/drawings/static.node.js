b5f14e1c3bdeefa374d5e6fe6a1116ef59d87a8c
caesarfeta <adamtavares@gmail.com>
2018-01-18 17:33:58 -0500

    working out node js templating

--- app/lib/drawings/static.node.js
+++ app/lib/drawings/static.node.js
@@ -4,6 +4,7 @@ var template = require( 'html-template' );
 var fs = require( 'fs' );
 var crypto = require('crypto');
 var rimraf = require('rimraf');
+var json2html = require('node-json2html');
 
 // config shit
 
@@ -15,43 +16,36 @@ if ( !fs.existsSync( dir )){
 
 // template
 
-function makeHtml( config ){
+function makeHtml( json, i ){
+  
+  var config = json[ i ]
+  
+  // next and previous
   
   // create id from filenames
   
-  var id = crypto.createHmac('sha256', 'abc')
+  config.id = crypto.createHmac('sha256', 'abc')
                  .update( config.files.join('') )
                  .digest('hex');
+                 
+  transform = {
+    "<>":"div",
+    "html": [
+      "${ id } ${ label } ${ description} ${ medium } ${ date } ${ files }"
+    ].join('')
+  };
   
-  fs.writeFile( dir + '/' + id + '.html', JSON.stringify( config ))
+  // build the html
+  
+  fs.writeFile(
+    dir + '/' + config.id + '.html',
+    json2html.transform( config, transform )
+  )
 }
 
 // load JSON file
 
 var json = JSON.parse( fs.readFileSync( "drawings.json" ));
 for ( var i in json ){
-  makeHtml( json[i] )
+  makeHtml( json, i )
 }
-
-// templating
-
-/*
-var html = template();
-var skills = html.template( 'skill' );
-fs.createReadStream( __dirname + '/static.html.tmpl' )
-    .pipe( html )
-    .pipe( process.stdout );
-skills.write({
-    '[key=name]': 'macaroni pictures',
-    '[key=level]': '40%'
-});
-skills.write({
-    '[key=name]': 'quilting',
-    '[key=level]': '5000%'
-});
-skills.write({
-    '[key=name]': 'blinky lights',
-    '[key=level]': '50%'
-});
-skills.end();
-*/
