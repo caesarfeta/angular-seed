fdb779010a810b641fdc994115353ddbea9c3a47
caesarfeta <adamtavares@gmail.com>
2018-05-11 21:39:32 -0400

    frottaging it

--- app/lib/frottage/frottage.js
+++ app/lib/frottage/frottage.js
@@ -22,15 +22,38 @@ function(
         ].join(' '),
         link: function( scope, elem ){
           var canvas = $( '#frottage', elem ).get( 0 )
-          var rows = 100
+          var colors = [ '#AAA', '#BBB', '#CCC', '#DDD', '#EEE', '#FFF' ]
+          var rows = 50
           var cols = 50
           var ix = canvas.height / rows
           var jx = canvas.width / cols
-          for ( var i=0; i<ix; i++ ){
-            for ( var j=0; j<jx; j++ ){
-              console.log( i, j )
+          var ctx = canvas.getContext( '2d' )
+          var grid = []
+          
+          function build(){
+            for ( var i=0; i<ix; i++ ){
+              grid[i]=[]
+              for ( var j=0; j<jx/2; j++ ){
+                grid[i][j] = colors[ Math.floor( Math.random() * colors.length )]
+              }
+              grid[i] = grid[i].concat( grid[i].slice().reverse() )
             }
           }
+          
+          function draw(){
+            for ( var i=0; i<ix; i++ ){
+              for ( var j=0; j<jx; j++ ){
+                ctx.beginPath()
+                ctx.rect( jx*j, ix*i, jx, ix )
+                ctx.fillStyle = grid[i][j]
+                ctx.fill()
+              }
+            }
+          }
+          
+          build()
+          draw()
+          
         }
       }
     }
